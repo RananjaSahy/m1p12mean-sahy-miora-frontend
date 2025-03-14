f6d3bdc242501981fcc08b03d799d3af358a01bb
Voninoliva <114289981+Voninoliva@users.noreply.github.com>
2025-03-14 23:30:33 +0300

    modifcation taille budget

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:5000' // URL pour le développement
-};
+    // apiUrl: 'http://localhost:5000' // URL pour le développement
+    apiUrl: 'm1p12mean-sahy-miora-backend-production.up.railway.app',
+
+  };
