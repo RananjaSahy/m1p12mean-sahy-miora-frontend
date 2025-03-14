d6a43889eee180df052509251b1effb21d3bca5e
Voninoliva <114289981+Voninoliva@users.noreply.github.com>
2025-03-14 22:13:11 +0300

    services fonctionnels cru

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  private apiUrl = 'http://localhost:5000/services';
-
-  constructor(private http: HttpClient) {}
-
+  private apiUrl = `${environment.apiUrl}/services`;
+  private token: string | null = null;
+  constructor(private http: HttpClient, private authService: AuthService) {
+    this.token = this.authService.getToken();
+  }
+  private getAuthHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`,
+      'Content-Type': 'application/json'
+    });
+  }
   // Récupérer tous les services
   getServices(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
@@ -17,11 +25,15 @@ export class ServiceService {
 
   // Ajouter un service
   addService(service: any): Observable<any> {
-    return this.http.post<any>(this.apiUrl, service);
+    return this.http.post<any>(this.apiUrl, service,{ headers: this.getAuthHeaders() });
   }
 
   // Ajouter une entrée dans l'historique d'un service
   addHistorique(serviceId: string, historique: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/${serviceId}/historique`, historique);
+    return this.http.post<any>(`${this.apiUrl}/${serviceId}/historique`, historique,{ headers: this.getAuthHeaders() });
   }
+  updateService(service: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${service._id}`, service,{ headers: this.getAuthHeaders() });
+  }
+
 }
