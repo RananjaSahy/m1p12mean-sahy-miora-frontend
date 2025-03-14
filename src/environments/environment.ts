7f66201b164c24151c9febda9d5ef123facc0e34
Voninoliva <114289981+Voninoliva@users.noreply.github.com>
2025-03-14 23:37:30 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:5000' // URL pour le développement
-    apiUrl: 'm1p12mean-sahy-miora-backend-production.up.railway.app',
+    apiUrl: 'http://localhost:5000' // URL pour le développement
 
   };
